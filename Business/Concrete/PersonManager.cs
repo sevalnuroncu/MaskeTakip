787df06153bb88cbfd6cea6fd1ae97017d115d4a
sevalnuroncu <sevalnuroncu@gmail.com>
2024-02-11 01:10:15 +0300

    Maske Takip projesi

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)//parametreyi peron olarak vermek encapsulation
+        {
+
+        }
+
+        public List<Person> GetList() 
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)//mernise bağlanır ve kişiyi kontrol eder
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(
+                new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalIdentity, person.FirstName, person.LastName, person.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
